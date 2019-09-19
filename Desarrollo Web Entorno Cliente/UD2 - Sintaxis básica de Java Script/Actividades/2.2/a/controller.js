69442d5f4ec510493e5a0c5fdba0606cc9db9563
nabby27 <ivancordoba77@gmail.com>
2019-09-19 19:08:44 +0200

    201909191908

--- Desarrollo Web Entorno Cliente/UD2 - Sintaxis básica de Java Script/Actividades/2.2/a/controller.js
+++ Desarrollo Web Entorno Cliente/UD2 - Sintaxis básica de Java Script/Actividades/2.2/a/controller.js
@@ -5,46 +5,50 @@ const CIRCLE_COLOR = 'rgba(200, 100, 100, 0.5)';
 const CROSS = 'X';
 const VOID = '_';
 
-paintBoard();
+paintInitBoard();
 
-function paintBoard() {
+function paintInitBoard() {
     for (let i = 0; i < 3; i++) {
         if (!board[i]) {
             board[i] = [];
         }
         for (let j = 0; j < 3; j++) {
-            board[i][j] = document.getElementById("cell_" + i + j).value  = VOID;
+            document.getElementById("cell_" + i + j).value = VOID;
+            board[i][j] = VOID;
         }
     }
     
     document.getElementById("cell_11").style.backgroundColor = CIRCLE_COLOR;
     board[1][1] = document.getElementById("cell_11").value  = CIRCLE;
+    document.getElementById("cell_11").disabled = true;
 }
 
 function changeRol() {
-    checkWinner();
     document.getElementById("button").disabled = true;
+    readHumanInput();
+    checkWinner();
+    selectRandomCell();    
+    checkWinner();
+    document.getElementById("button").disabled = false;
+}
 
-    voidCells = [];
+function readHumanInput() {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            if (document.getElementById("cell_" + i + j).value == '_') {
-                voidCells.push([i, j]);
-            }
-            board[i][j] = document.getElementById("cell_" + i + j).value;
+            rewriteCharacterToCross(i, j);
         }
     }
-    selectedIndex = Math.floor(Math.random() * voidCells.length);
-    selectedCel = voidCells[selectedIndex];
+}
 
-    document.getElementById("cell_" + selectedCel[0] + selectedCel[1]).value = CIRCLE;
-    document.getElementById("cell_" + selectedCel[0] + selectedCel[1]).disabled = true;
-    document.getElementById("cell_" + selectedCel[0] + selectedCel[1]).style.backgroundColor = CIRCLE_COLOR;
-    board[selectedCel[0]][selectedCel[1]] = CIRCLE;
+function rewriteCharacterToCross(i, j) {
+    cellValue = document.getElementById("cell_" + i + j).value;
 
-    document.getElementById("button").disabled = false;
-    
-    checkWinner();
+    if (cellValue == VOID || cellValue == CIRCLE || cellValue == CROSS) {
+        board[i][j] = document.getElementById("cell_" + i + j).value;
+    } else {
+        board[i][j] = CROSS;
+        document.getElementById("cell_" + i + j).value = CROSS;
+    }
 }
 
 function checkWinner() {
@@ -52,10 +56,10 @@ function checkWinner() {
     endGame = true;
     for (let i = 0; i < 3 && !success; i++) {
         for (let j = 0; j < 3 && !success; j++) {
-            if (checkRow(i, j) ||
-                checkColumn(i, j) || 
-                checkDiagonal(i, j) ||
-                checkReverseDiagonal(i, j)) {
+            if (checkSameItemOnRow(i, j) ||
+                checkSameItemOnColumn(i, j) || 
+                checkSameItemOnDiagonal(i, j) ||
+                checkSameItemOnReverseDiagonal(i, j)) {
                     success = true;
                     winner = board[i][j];
             }
@@ -72,21 +76,69 @@ function checkWinner() {
     } else if (endGame) {
         alert("Empate");
     }
+
+    return success;
     
 }
 
-function checkRow(i, j) {
-    return (i+2 < 3 || j+2 < 3) && board[i][j] != VOID && board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2];
+function selectRandomCell() {
+    voidCells = getVoidCells();
+    selectedIndex = Math.floor(Math.random() * voidCells.length);
+    selectedCel = voidCells[selectedIndex];
+    printSelectedCell(selectedCel);
 }
 
-function checkColumn(i, j) {
-    return (i+2 < 3 || j+2 < 3) && board[i][j] != VOID && board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j];
+function getVoidCells() {
+    voidCells = [];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (document.getElementById("cell_" + i + j).value == '_') {
+                voidCells.push([i, j]);
+            }
+        }
+    }
+    return voidCells;
 }
 
-function checkDiagonal(i, j) {
-    return (i == 0 || j == 0) && board[i][j] != VOID && board[i][j] == board[i+1][j+1] && board[i][j] == board[i+2][j+2];
+function printSelectedCell(selectedCel) {
+    document.getElementById("cell_" + selectedCel[0] + selectedCel[1]).value = CIRCLE;
+    document.getElementById("cell_" + selectedCel[0] + selectedCel[1]).disabled = true;
+    document.getElementById("cell_" + selectedCel[0] + selectedCel[1]).style.backgroundColor = CIRCLE_COLOR;
+    board[selectedCel[0]][selectedCel[1]] = CIRCLE;
 }
 
-function checkReverseDiagonal(i, j) {
-    return (i == 0 || j == 2) && board[i][j] != VOID && board[i][j] == board[i-1][j-1] && board[i][j] == board[i-2][j-2];
+function checkSameItemOnRow(i, j) {
+    isSameItemOnRow = false;
+    if (j == 0) {
+        isSameItemOnRow =  board[i][j] != VOID && board[i][j] == board[i][j+1] && board[i][j] == board[i][j+2];
+    }
+
+    return isSameItemOnRow;
+}
+
+function checkSameItemOnColumn(i, j) {
+    isSameItemOnColumn = false;
+    if (i == 0) {
+        isSameItemOnColumn = board[i][j] != VOID && board[i][j] == board[i+1][j] && board[i][j] == board[i+2][j];
+    }
+
+    return isSameItemOnColumn;
+}
+
+function checkSameItemOnDiagonal(i, j) {
+    isSameItemOnDiagonal = false;
+    if (i == 0 && j == 0) {
+        isSameItemOnDiagonal = board[i][j] != VOID && board[i][j] == board[i+1][j+1] && board[i][j] == board[i+2][j+2];
+    }
+
+    return isSameItemOnDiagonal;
+}
+
+function checkSameItemOnReverseDiagonal(i, j) {
+    isSameItemOnReverseDiagonal = false;
+    if (i == 0 && j == 2) {
+        isSameItemOnReverseDiagonal = board[i][j] != VOID && board[i][j] == board[i+1][j-1] && board[i][j] == board[i+2][j-2];
+    }
+
+    return isSameItemOnReverseDiagonal;
 }

6bcc57f11bfb94344b6bea253d1cb1760e3ecea6
nabby27 <ivancordoba77@gmail.com>
2020-02-09 12:22:02 +0100

    202002091222

--- PMD/2 EVAL/database/virtualmarket_ivan.sql
+++ PMD/2 EVAL/database/virtualmarket_ivan.sql
@@ -7,10 +7,13 @@
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
+CREATE DATABASE virtualmarket_ivan;
+USE virtualmarket_ivan;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+01:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -57,7 +60,7 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `dni_client` varchar(9) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -174,3 +177,4 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
